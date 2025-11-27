64a483ed5a072818899729f7f379592f66fb7c2e
MatiXxD <ilya_dinesenko@mail.ru>
2025-11-28 01:46:54 +0300

    fix: fix tokenize

--- internal/processor/service/tokenize.go
+++ internal/processor/service/tokenize.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/keenywheels/backend/internal/pkg/client/llm"
 	tokenizerbase "github.com/keenywheels/backend/internal/pkg/tokenizer"
-	"github.com/keenywheels/backend/internal/pkg/tokenizer/metrics"
 	"github.com/keenywheels/backend/internal/processor/models"
 	"github.com/keenywheels/backend/pkg/ctxutils"
 )
@@ -23,8 +22,11 @@ func (s *Service) TokenizeMessage(ctx context.Context, message string) error {
 		return fmt.Errorf("[%s] failed to unmarshal message: %w", op, err)
 	}
 
+	// create tokenizer pipeline
+	tokenizer, registry := getTokenizer()
+
 	// tokenize msg
-	tokens := s.tokenizer.Run(tokenizerbase.GetTokens(
+	tokens := tokenizer.Run(tokenizerbase.GetTokens(
 		scraperEvent.Msg,
 		tokenizerbase.NewTokenConfig(
 			tokenizerbase.DefaultTokenSource,
@@ -32,7 +34,7 @@ func (s *Service) TokenizeMessage(ctx context.Context, message string) error {
 		),
 	))
 
-	tokensModel, err := s.parseTokens(ctx, &scraperEvent, tokens)
+	tokensModel, err := s.parseTokens(ctx, &scraperEvent, tokens, registry)
 	if err != nil {
 		return fmt.Errorf("[%s] failed to parse tokens: %w", op, err)
 	}
@@ -50,6 +52,7 @@ func (s *Service) parseTokens(
 	ctx context.Context,
 	msg *models.ScraperEvent,
 	tokens []tokenizerbase.Token,
+	registry metricsRegistry,
 ) ([]models.TokenData, error) {
 	var (
 		log    = ctxutils.GetLogger(ctx)
@@ -63,42 +66,55 @@ func (s *Service) parseTokens(
 		return nil, fmt.Errorf("failed to parse scrape date %s: %w", date, err)
 	}
 
+	uniqRes := make(map[string]int64)
+	tokensContext := make(map[string]*strings.Builder)
+
 	for _, t := range tokens {
 		// skip filtered tokens
 		if t.IsFiltered() {
 			continue
 		}
 
-		// parse token's context
-		tokenContext := make([]string, len(t.Context))
-		for i, ctxToken := range t.Context {
-			tokenContext[i] = ctxToken.Target
+		// add token if not exists
+		if _, ok := uniqRes[t.Target]; !ok {
+			tokensContext[t.Target] = new(strings.Builder) // initialize context builder
+
+			interest, ok := registry[interestMetricKey].Get(t.Target)
+			if !ok {
+				log.Errorf("failed to get interest metric for token %s", t.Target)
+				continue
+			}
+
+			interestInt, ok := interest.(int64)
+			if !ok {
+				log.Errorf("interest metric has invalid type for token %s: %T", t.Target, interest)
+				continue
+			}
+
+			uniqRes[t.Target] = interestInt
 		}
 
+		// append tokens context
+		for _, ctxToken := range t.Context {
+			tokensContext[t.Target].WriteString(ctxToken.Target + " ")
+		}
+		tokensContext[t.Target].WriteString("; ")
+	}
+
+	// make final result
+	for tokenName, interest := range uniqRes {
+		// analyze sentiment
 		resp, err := s.llm.SentimentAnalysis(ctx, &llm.SentimentAnalysisRequest{
-			Context: strings.Join(tokenContext, " "),
+			Context: tokensContext[tokenName].String(),
 		})
 		if err != nil {
-			return nil, fmt.Errorf("failed to analyze sentiment for token %s: %w", t.Target, err)
-		}
-
-		// get metrics
-		interest, ok := metrics.Registry[metrics.InterestMetricIndex].Get(t.Target)
-		if !ok {
-			log.Errorf("failed to get interest metric for token %s", t.Target)
-			continue
-		}
-
-		interestInt, ok := interest.(int64)
-		if !ok {
-			log.Errorf("interest metric has invalid type for token %s: %T", t.Target, interest)
-			continue
+			return nil, fmt.Errorf("failed to analyze sentiment for token %s: %w", tokenName, err)
 		}
 
 		// append to result
 		result = append(result, models.TokenData{
-			TokenName: t.Target,
-			Interest:  interestInt,
+			TokenName: tokenName,
+			Interest:  interest,
 			Sentiment: resp.Sentiment,
 			SiteName:  site,
 			Date:      dateParsed,

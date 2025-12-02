53df0d56fd0fa0a5129ad7b58a50e5bbdbb9aa6b
MatiXxD <ilya_dinesenko@mail.ru>
2025-12-03 01:52:03 +0300

    feat: add user migrations

---
+++ migrations/20251202224058_user.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+  id UUID DEFAULT gen_random_uuid(),
+  username TEXT NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  vkid INTEGER UNIQUE,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+
+  CONSTRAINT id_pkey PRIMARY KEY(id)
+);
+
+COMMENT ON COLUMN users.id IS 'Идентификатор пользователя';
+COMMENT ON COLUMN users.username IS 'Имя пользователя';
+COMMENT ON COLUMN users.email IS 'Электронная почта пользователя';
+COMMENT ON COLUMN users.vkid IS 'Идентификатор пользователя в vk';
+COMMENT ON COLUMN users.created_at IS 'Дата и время создания пользователя';
+
+CREATE INDEX users_email_idx ON users(email);
+CREATE INDEX users_vkid_idx ON users(vkid) WHERE vkid IS NOT NULL;

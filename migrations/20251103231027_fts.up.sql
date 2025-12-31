7e9f017922d8eb4e4022bf242e87c2d199b38c22
MatiXxD <ilya_dinesenko@mail.ru>
2025-12-31 17:17:18 +0300

    perf: improve search mv query + format sql

--- migrations/20251103231027_fts.up.sql
+++ migrations/20251103231027_fts.up.sql
@@ -1,26 +1,24 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
 CREATE MATERIALIZED VIEW mv_token_search AS
-WITH aggr AS (
-  SELECT
-    token_name,
-    scrape_date,
-    SUM(interest) AS interest,
-    ROUND(AVG(sentiment))::SMALLINT AS sentiment
-  FROM token_data
-  GROUP BY (token_name, scrape_date)
-)
-SELECT
-  token_name,
-  scrape_date,
-  interest,
-  sentiment,
-  (SELECT
-    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY interest)
-    FROM aggr aggr2
-    WHERE aggr2.scrape_date = aggr.scrape_date
-  ) AS median_interest
-FROM aggr;
+WITH
+    aggr AS (SELECT token_name,
+                    scrape_date,
+                    SUM(interest)                   AS interest,
+                    ROUND(AVG(sentiment))::SMALLINT AS sentiment
+             FROM token_data
+             GROUP BY (token_name, scrape_date)),
+    medians AS (SELECT scrape_date,
+                       PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY interest) AS median_interest
+                FROM aggr
+                GROUP BY scrape_date)
+SELECT a.token_name,
+       a.scrape_date,
+       a.interest,
+       a.sentiment,
+       m.median_interest
+FROM aggr a
+         JOIN medians m ON a.scrape_date = m.scrape_date;
 
 CREATE UNIQUE INDEX mv_token_search_pk ON mv_token_search (token_name, scrape_date);
 CREATE INDEX mv_token_search_trgm_idx ON mv_token_search USING GIN (token_name gin_trgm_ops);

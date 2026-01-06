85d6eca9636ea4dccfd3a05e91dcaf2f0137f90d
MatiXxD <ilya_dinesenko@mail.ru>
2026-01-06 22:39:44 +0300

    refactor: improve user_token_sub table scheme

--- migrations/20260106152609_alerts.up.sql
+++ migrations/20260106152609_alerts.up.sql
@@ -7,11 +7,14 @@ CREATE TABLE user_token_sub
     curr_interest BIGINT      NOT NULL,
     prv_interest  BIGINT      NOT NULL DEFAULT 0,
     threshold     NUMERIC     NOT NULL,
+    method        TEXT        NOT NULL DEFAULT 'denormalized',
     updated_at    TIMESTAMPTZ NOT NULL,
 
     CONSTRAINT user_token_sub_pk PRIMARY KEY (id),
     CONSTRAINT user_token_sub_user_id_fkey FOREIGN KEY (user_id)
-        REFERENCES users (id) ON DELETE CASCADE
+        REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT user_token_sub_method_check
+        CHECK (method IN ('denormalized', 'global_median', 'category_median'))
 );
 
 COMMENT ON COLUMN user_token_sub.id IS 'Идентификатор подписки пользователя на токен';
@@ -21,8 +24,9 @@ COMMENT ON COLUMN user_token_sub.category IS 'Категория токена';
 COMMENT ON COLUMN user_token_sub.curr_interest IS 'Текущий интерес по токену';
 COMMENT ON COLUMN user_token_sub.prv_interest IS 'Предыдущее значение интереса по токену';
 COMMENT ON COLUMN user_token_sub.threshold IS 'Пороговое значение для уведомления';
+COMMENT ON COLUMN user_token_sub.method IS 'Метод для сравнение значений интереса';
 COMMENT ON COLUMN user_token_sub.updated_at IS 'Дата и время последнего обновления';
 
-CREATE UNIQUE INDEX user_token_sub_token_user_idx ON user_token_sub (user_id, category, token);
+CREATE UNIQUE INDEX user_token_sub_unique_idx ON user_token_sub (user_id, category, token, method);
 CREATE INDEX user_token_sub_user_id_idx ON user_token_sub (user_id);
 CREATE INDEX user_token_sub_updated_at_idx ON user_token_sub (updated_at);
